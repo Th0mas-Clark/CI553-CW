e2b8fc4cacdfcb70bbd03e7f435aef10cca3aa47
bazcl <baz.clark@outlook.com>
2023-11-23 23:19:54 +0000

    Added Junit test for BetterBasket extension

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -4,15 +4,28 @@ import java.io.Serializable;
 import java.util.Collections;
 
 /**
- * Write a description of class BetterBasket here.
+ * Better
  * 
- * @author  Your Name 
+ * @author  Thomas Clark
  * @version 1.0
  */
 public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
-  
 
-  // You need to add code here
+  @Override
+  public boolean add( Product p1 ) {
+    //search existing products for matching record
+    for (Product p2: this) {
+      if (p1.getProductNum().equals(p2.getProductNum())) {
+        //found - updates quantity and returns
+        p2.setQuantity(p2.getQuantity()+p1.getQuantity());
+        return(true);
+      }
+
+  }
+    // not found - adds new product, using superclass method
+    super.add(p1); // Call add in ArrayList
+    return(true);
+  }
 }
